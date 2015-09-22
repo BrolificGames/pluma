fb3c9b78bb5951695694d0bff735b99bb3171b28
Nathan Kluth <nlkluth@gmail.com>
2015-09-22 15:31:34 -0600

    adding character sript

---
+++ pluma/Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
